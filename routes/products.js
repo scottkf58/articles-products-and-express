ef63c0465b20c4b3061ae6f7a22684a020d91eb6
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-07-15 18:42:46 -1000

    adding routes

---
+++ routes/products.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.route( '/' )
+  .get( ( req, res ) => {
+
+  } )
+  .post( ( req, res ) => {
+
+  } )
+  .put( ( req, res ) => {
+  } )
+  .delete( ( req, res ) => {
+  } )
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const expbhs = require('express-handlebars');
 const app = express();
+const articleRoute = require('./routes/articles.js');
+const productRoute = require('./routes/products.js');
+const bodyParser = require('body-parser');
 
 
+app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
-const hbs = expbhs.create({
-  extname: 'hbs',
-  defaultLayout: 'main'
-})
+app.use('/articles', articleRoute);
+app.use('/products', productRoute);
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
+// const hbs = expbhs.create({
+//   extname: 'hbs',
+//   defaultLayout: 'main'
+// })
+
+// app.engine('hbs', hbs.engine);
+// app.set('view engine', 'hbs');
+
+
+
+
 
 
 
