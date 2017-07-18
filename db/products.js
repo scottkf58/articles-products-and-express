cbe716e51ad5c4b924eb45a2531734072c9537ee
BK <bkam03@yahoo.com>
2017-07-17 18:16:58 -1000

    created structure of product database.

---
+++ db/products.js
@@ -0,0 +1,39 @@
+( function(){
+  const productArray = [];
+
+  function getProductById( id ){
+
+  }
+
+  function updateProduct( id ){
+
+  }
+
+  function addProduct( product ){
+
+  }
+
+  function deletProduct( id ){
+
+  }
+
+  function getAllProducts(){
+
+  }
+
+  function getProduct( id ){
+
+  }
+
+
+
+  module.exports = {
+    getProductById,
+    updateProduct,
+    addProduct,
+    deletProduct,
+    getAllProducts,
+    getProduct
+  };
+
+} )();
