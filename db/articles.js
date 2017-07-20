533e37b3aec1648baf641a1c036d381e41174bc4
BK <bkam03@yahoo.com>
2017-07-19 21:40:45 -1000

    copied scott changes

--- db/articles.js
+++ db/articles.js
@@ -1,4 +1,6 @@
 const collection = [];
+var article;
+var index;
 
 function getArticleByTitle (title) {
   for(var i = 0; i < collection.length; i++) {
@@ -9,32 +11,52 @@ function getArticleByTitle (title) {
 return false;
 }
 
-function updateArticle () {
+// Post request
+function add (newArt) {
+  let tempArt = {
+    title : newArt.title,
+    body : newArt.body,
+    author : newArt.author,
+    urlTitle : encodeURI(newArt.title)
+  };
 
 }
 
-function addArticle () {
-
-}
-
-function deleteArticle () {
-
-}
-
+// Get request
 function getAll () {
   return collection;
 }
 
-function getArticle () {
+// Put request
+function editArticle () {
 
+}
+
+// Delete article
+function deleteArticle (obj) {
+  collection.forEach( (element) => {
+    if(obj.params.title === element.title) {
+      index = collection.indexOf(element);
+      collection.splice(index, 1);
+    }
+  })
+}
+
+// Get request
+function getArticle (obj) {
+  collection.forEach( (element) => {
+    if(obj.params.title === element.title) {
+      article = element;
+    }
+  })
 }
 
 
 module.exports = {
   getArticleByTitle: getArticleByTitle,
-  updateArticle: updateArticle,
-  addArticle: addArticle,
-  deleteArticle: deleteArticle,
+  add: add,
   getAll: getAll,
+  editArticle: editArticle,
+  deleteArticle: deleteArticle,
   getArticle: getArticle
 };
