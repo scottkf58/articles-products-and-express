3c64aec0aa6f8f58ad52b09d53b9fadd73e003e2
BK <bkam03@yahoo.com>
2017-08-03 19:11:34 -1000

    added articles/ route

--- db/database.js
+++ db/database.js
@@ -6,13 +6,63 @@ const {DATABASE, USER, PASSWORD} = require('../config/config.json');
 const connectionOptions = {
   host: 'localhost',
   port: 5432,
-  database: CONFIG.DATABASE,
-  user: CONFIG.USER,
-  password: CONFIG.PASSWORD
+  database: DATABASE,
+  user: USER,
+  password: PASSWORD
 };
 
 const db = pgp(connectionOptions);
 
-module.exports = {
+// Post request
+function addArticle (newArt) {
+  let tempArt = {
+    title : newArt.title,
+    body : newArt.body,
+    author : newArt.author,
+    urlTitle : encodeURI(newArt.title)
+  };
+  collection.push(tempArt);
+}
 
+// Get request
+function getAllArticles () {
+  return db.query('SELECT * FROM articles');
+}
+
+// Put request
+function editArticle (title, obj) {
+  collection.forEach( (element) => {
+    if(element.title === title) {
+      element.title = obj.title;
+      element.body = obj.body;
+      element.author = obj.author;
+      element.urlTitle = obj.urlTitle;
+      //console.log(element);
+    }
+  })
+}
+
+// Delete article
+function deleteArticle (title) {
+  collection.forEach( (element) => {
+    if(title === element.title) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+    }
+  })
+}
+
+// Get request
+function getArticle (title) {
+  var articleIndex = getArticleByTitle(title);
+  return collection[articleIndex];
+}
+
+
+module.exports = {
+  addArticle: addArticle,
+  getAllArticles: getAllArticles,
+  editArticle: editArticle,
+  deleteArticle: deleteArticle,
+  getArticle: getArticle
 };
